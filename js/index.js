0b9f335f8150a4dfb596f80e01fbc247c4d18d9e
raulpesilva <adraulpesilva@gmail.com>
2019-02-24 20:03:11 -0300

    area tirar menus

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const $fecharMenu = document.querySelector('.fechar-menu');
 const $botaoAdicionarTarefa = document.querySelector('.botao-adicionar-tarefa');
 const $conteinerTarefa = document.querySelector('.container-tarefa');
 const $fecharTarefa = document.querySelector('.menu-hide-dicionar-tarefa');
+const $overlayMenu = document.querySelector('.overlay-menu');
 
 
 // function sairMenu(botaoApertado){
@@ -15,20 +16,41 @@ const $fecharTarefa = document.querySelector('.menu-hide-dicionar-tarefa');
 
 $botaoMenu.addEventListener('click',function(event){
   // sairMenu(event.target);
-  $menuLateral.classList.toggle('menu-lateral-ativo');
+  abreFechaMenu();
   // if (event.target === $botaoMenu || event.target === $iconeMenu){
   // }
 });
 $fecharMenu.addEventListener('click', event => {
-  $menuLateral.classList.remove('menu-lateral-ativo');
+  abreFechaMenu();
 
 });
 
 $botaoAdicionarTarefa.addEventListener('click', event =>{
-  $conteinerTarefa.classList.add('mostra-tarefa');
+  abreFechaTarefa();
 });
 
 $fecharTarefa.addEventListener('click', event =>{
-  $conteinerTarefa.classList.remove('mostra-tarefa');
+  abreFechaTarefa();
 
 });
+
+$overlayMenu.addEventListener('click',event=>{
+  fechaMenu();
+
+});
+
+function fechaMenu(){
+  $conteinerTarefa.classList.remove('mostra-tarefa');
+  $overlayMenu.classList.remove('overlay-menu-ativo');
+  $menuLateral.classList.remove('menu-lateral-ativo');
+}
+
+function abreFechaTarefa(){
+  $conteinerTarefa.classList.toggle('mostra-tarefa');
+  $overlayMenu.classList.toggle('overlay-menu-ativo');
+}
+
+function abreFechaMenu(){
+  $menuLateral.classList.toggle('menu-lateral-ativo');
+  $overlayMenu.classList.toggle('overlay-menu-ativo');
+}
