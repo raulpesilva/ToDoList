b0643024f075b37695222cc9b1fbf1cbe2aeed48
raulpesilva <adraulpesilva@gmail.com>
2019-02-24 14:56:39 -0300

    menu tarefa

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const $menuLateral = document.querySelector('#menu-lateral');
 const $botaoMenu = document.querySelector('.botao-menu');
 const $fecharMenu = document.querySelector('.fechar-menu');
+const $botaoAdicionarTarefa = document.querySelector('.botao-adicionar-tarefa');
+const $conteinerTarefa = document.querySelector('.container-tarefa');
+const $fecharTarefa = document.querySelector('.menu-hide-dicionar-tarefa');
 
 
 // function sairMenu(botaoApertado){
@@ -18,5 +21,14 @@ $botaoMenu.addEventListener('click',function(event){
 });
 $fecharMenu.addEventListener('click', event => {
   $menuLateral.classList.remove('menu-lateral-ativo');
+
+});
+
+$botaoAdicionarTarefa.addEventListener('click',event=>{
+  $conteinerTarefa.classList.add('mostra-tarefa');
+});
+
+$fecharTarefa.addEventListener('click',event=>{
+  $conteinerTarefa.classList.remove('mostra-tarefa');
 
 })
