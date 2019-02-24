06e4329a5c0860fbc3ade29008229029fbf25cc0
raulpesilva <adraulpesilva@gmail.com>
2019-02-24 14:36:40 -0300

    ajuste menu

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 const $menuLateral = document.querySelector('#menu-lateral');
 const $botaoMenu = document.querySelector('.botao-menu');
-const $iconeMenu = document.querySelector('.botao-menu-icone');
+const $fecharMenu = document.querySelector('.fechar-menu');
 
 
-function sairMenu(botaoApertado){
-  const $menuLateralAtivo =  document.querySelector('#menu-lateral');
-  if ($menuLateralAtivo.classList.contains('menu-lateral-ativo') && botaoApertado !== $botaoMenu){
-    $menuLateral.classList.remove('menu-lateral-ativo');
-  }
-}
+// function sairMenu(botaoApertado){
+//   const $menuLateralAtivo =  document.querySelector('#menu-lateral');
+//   if ($menuLateralAtivo.classList.contains('menu-lateral-ativo') && botaoApertado !== $botaoMenu){
+//     $menuLateral.classList.remove('menu-lateral-ativo');
+//   }
+// }
 
-window.addEventListener('click',function(event){
-  sairMenu(event.target);
-  if (event.target === $botaoMenu || event.target === $iconeMenu){
-    $menuLateral.classList.toggle('menu-lateral-ativo');
-  }
+$botaoMenu.addEventListener('click',function(event){
+  // sairMenu(event.target);
+  $menuLateral.classList.toggle('menu-lateral-ativo');
+  // if (event.target === $botaoMenu || event.target === $iconeMenu){
+  // }
 });
+$fecharMenu.addEventListener('click', event => {
+  $menuLateral.classList.remove('menu-lateral-ativo');
+
+})
